62be66f15190853f0550c8e6bd57d27a1301df32
Natali <natali.dnp.ua@gmail.com>
2023-05-03 19:59:54 +0300

    fix env

--- main.py
+++ main.py
@@ -1,20 +1,10 @@
 from application.fuzz_generator.fuzz_generator import concurrency_fuzz_generator
 from application.logging.init_logging import init_logging
-import os
 
 
 def main() -> None:
 
     concurrency_fuzz_generator()
-
-    dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
-    if not os.path.exists(dotenv_path):
-        env_template_path = os.path.join(os.path.dirname(__file__), ".env.template")
-        with open(env_template_path) as f:
-            env_template = f.read()
-
-        with open(dotenv_path, "w") as f:
-            f.write(env_template)
 
 
 if __name__ == "__main__":
